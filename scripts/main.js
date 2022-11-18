4054f6f541a1b8347da8ec010f0d8be26ed8a51c
Ankita Joshi <joshiankita445@gmail.com>
2022-11-18 08:38:16 +0530

    add simple website structure

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+//headding code
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+
+  
+//image code
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+//button code
+let myButton = document.querySelector('button');
+myButton.onclick = function() {
+    setUserName();
+  }
+  
